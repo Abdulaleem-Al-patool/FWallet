fc393e0501393aaddd9902a32432859a9f7b8575
Abdulaleem Al-Patool <ab0odsoftwar@gmail.com>
2026-09-05 23:56:01 +0300

    Modify Data anf add [token,data] (0_^)

--- src/shared/utils/FetchData.jsx
+++ src/shared/utils/FetchData.jsx
@@ -1,19 +1,39 @@
 import { httpClient } from "./HttClient";
+
+// export async function fetchData(url,token){
+//     try{
+//         const response =await httpClient.get(url,token);
+//         if (!Array.isArray(response)) {
+//             return response;
+//         }
+
+//         const isTokenWrapped = response.some(item => item?.token);
+//         if (!isTokenWrapped) {
+//             return response;
+//         }
+
+//         const user = response.find(item => item.token === token);
+//         return user?.data ?? [];
+//     }catch(error){
+//         return null;
+//     }
+// }
+
+
+
 export async function fetchData(url,token){
     try{
         const response =await httpClient.get(url,token);
-        if (!Array.isArray(response)) {
-            return response;
-        }
-
-        const isTokenWrapped = response.some(item => item?.token);
-        if (!isTokenWrapped) {
-            return response;
-        }
-
-        const user = response.find(item => item.token === token);
-        return user?.data ?? [];
+        console.log(`ItemToken:${token}`)
+        console.log(response)
+        console.log(`url:${url}`)
+        console.log(Array.isArray(response))
+        console.log(response.find(item=>item.token===token))
+        const user=Array.isArray(response)? response.find(item=>item.token===token):null;
+        console.log(user)
+        return user?.data;  
     }catch(error){
+        console.log(error)
         return null;
     }
 }
